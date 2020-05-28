d98cf6bbb0a65a76334083536cc2c6223f9ad852
zhangzq7 <zhangzq7@asiainfo.com>
2020-05-28 21:10:45 +0800

    springboot_prometheus

---
+++ springboot_prometheus/src/main/java/com/oven/Application.java
@@ -0,0 +1,30 @@
+package com.oven;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class Application {
+
+    @RequestMapping("/test")
+    public String test(String name) {
+        return "hello " + name;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public MeterRegistryCustomizer<MeterRegistry> configurer(@Value("${spring.application.name}") String applicationName) {
+        return (registry) -> registry.config().commonTags("application", applicationName);
+    }
+
+}
