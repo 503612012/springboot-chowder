71c7a87ca0ba4c4d371cbe6644005755f6156ec0
zhangzq7 <zhangzq7@asiainfo.com>
2020-11-02 17:35:28 +0800

    springboot_redis

--- springboot_redis/src/main/java/com/oven/config/RedisConfig.java
+++ springboot_redis/src/main/java/com/oven/config/RedisConfig.java
@@ -4,12 +4,8 @@ import com.alibaba.fastjson.support.spring.GenericFastJsonRedisSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-
-import java.io.Serializable;
 
 /**
  * redis配置类
